85df8b114208c5470b706970d35767413c3247c7
Laura Logan <laurardh@aol.com>
2017-08-31 15:28:47 -0400

    changed build code to jq- not working

--- js/script.js
+++ js/script.js
@@ -15,32 +15,39 @@ var cats = [{
 function addCats(cats) {
   for(var i = 0; i < cats.length; i++) {
     //creates div to hold each "cat block"(name, image, counter)
-    var catElem = document.createElement('div');
-    catElem.className = ("catElem");
+    var catElem = $('body').append('<div></div>');
+    //document.createElement('div');
+    $('catElem').addClass('catElem');
+    //catElem.className = ('catElem');
     //adds cat name to document
-    var catName = document.createElement('h3');
-    catName.innerHTML = cats[i].name;
-    catElem.appendChild(catName);
+    var catName = $('catElem').append('<h3></h3>');
+    //document.createElement('h3');
+    $('catName').html('cats[i].name');
+    //$(catElem).append(catName);
 
     //adds cat image to document
-    var catImage = document.createElement('img');
-    catImage.src = cats[i].img;
-    catImage.style.width = '200px';
-    catImage.className = ("catImg");
-    catElem.appendChild(catImage);
+    var catImage = $('body').append('<img></img>');
+    //document.createElement('img');
+    $('catImage').attr('src', 'cats[i].img');
+    $('catImage').width('200px');
+    //style.width = '200px';
+    $('catImage').addClass('catImg');
+    $('catElem').append('catImage');
 
     // added this to better visualize the console.logged element
-    catImage.id = "image-" + cats[i].id;
+    $('catImage').attr('id', 'image-' + 'cats[i].id');
 
     //adds counter to document
-    var catCounter = document.createElement('h4');
+    var catCounter = $('body').append('<h4></h4>');
+    //document.createElement('h4');
     // added this to better visualize the console.logged element
-    catCounter.id = "counter-" + cats[i].id
+    $('catCounter').attr('id', 'counter-' + 'cats[i].id');
 
-    catCounter.innerHTML = cats[i].count;
-    catElem.appendChild(catCounter);
+    $('catCounter').html('cats[i].count');
+    $('catElem').append(catCounter);
 
-    document.body.appendChild(catElem);
+    $('body').append(catElem);
+    //document.body.appendChild(catElem);
   };
 };
 addCats(cats);
@@ -48,9 +55,9 @@ addCats(cats);
 
 $(document).ready(function() {
  $('img').click(function(){
-   var tally = parseInt($(this).next("h4").text());
+   var tally = parseInt($(this).next('h4').text());
    tally++;
-    $(this).next("h4").text(tally++)
+    $(this).next('h4').text(tally++)
    console.log(tally);
    console.log(this);
  });
