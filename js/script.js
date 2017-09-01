109b9613f3e2d34afd7276d02fde1a2eb3b669cb
Laura Logan <laurardh@aol.com>
2017-09-01 09:49:42 -0400

    edits jq syntax for creating elements and appending

--- js/script.js
+++ js/script.js
@@ -15,38 +15,45 @@ var cats = [{
 function addCats(cats) {
   for(var i = 0; i < cats.length; i++) {
     //creates div to hold each "cat block"(name, image, counter)
-    var catElem = $('body').append('<div></div>');
+    var catElem = document.createElement('div');
     //document.createElement('div');
-    $('catElem').addClass('catElem');
+      $('catElem').addClass('catElem');
+      $('body').append(catElem);
+
+
     //catElem.className = ('catElem');
     //adds cat name to document
-    var catName = $('catElem').append('<h3></h3>');
+    var catName = document.createElement('h3');
     //document.createElement('h3');
-    $('catName').html('cats[i].name');
-    //$(catElem).append(catName);
+    $(catElem).append('<h3></h3>');
+
+    $(catName).html(cats[i].name);
+    $(catElem).append(catName);
 
     //adds cat image to document
-    var catImage = $('body').append('<img></img>');
-    //document.createElement('img');
-    $('catImage').attr('src', 'cats[i].img');
-    $('catImage').width('200px');
+    var catImage = document.createElement('img');
+        //document.createElement('img');
+    $('body').append('<img></img>');
+
+    $(catImage).attr('src', 'cats[i].img');
+    $(catImage).width('200px');
     //style.width = '200px';
-    $('catImage').addClass('catImg');
-    $('catElem').append('catImage');
+    $(catImage).addClass('catImg');
+    $(catElem).append(catImage);
 
     // added this to better visualize the console.logged element
-    $('catImage').attr('id', 'image-' + 'cats[i].id');
+    $(catImage).attr('id', 'image-' + 'cats[i].id');
 
     //adds counter to document
     var catCounter = $('body').append('<h4></h4>');
     //document.createElement('h4');
     // added this to better visualize the console.logged element
-    $('catCounter').attr('id', 'counter-' + 'cats[i].id');
+    $(catCounter).attr('id', 'counter-' + 'cats[i].id');
 
-    $('catCounter').html('cats[i].count');
-    $('catElem').append('catCounter');
+    $(catCounter).html(cats[i].count);
+    $(catElem).append(catCounter);
 
-    $('body').append('catElem');
+    $('body').append(catElem);
     //document.body.appendChild(catElem);
   };
 };
